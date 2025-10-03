a0970da495d3bc5fb09edab49f75392146e50a4e
mahal <mahal@kumaran1415>
2025-10-03 16:53:17 +0530

    COMMIT

---
+++ Corejavaprograms/src/Day7/overriding/covariant/v1/HDFC.java
@@ -0,0 +1,8 @@
+package Day7.overriding.covariant.v1;
+
+public class HDFC {
+	public float getRateOfInterest() {
+		return 6.8f;
+	}
+
+}
---
+++ Corejavaprograms/src/Day7/overriding/covariant/v1/SBI.java
@@ -0,0 +1,8 @@
+package Day7.overriding.covariant.v1;
+
+public class SBI {
+	public float getRateOfInterest() {
+		return 6.7f;
+	}
+
+}
