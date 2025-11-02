8fe7972471d691c93ac83ed759afa4af39df059f
mahal <mahal@kumaran1415>
2025-11-02 18:55:22 +0530

    commit

--- Corejavaprograms/src/day14/synchronization/Bank.java
+++ Corejavaprograms/src/day14/synchronization/Bank.java
@@ -1,9 +1,36 @@
 package day14.synchronization;
+//Create a custom exception
+class Bank extends Exception {
+ public DepositLimitExceedsException(String message) {
+     super(message);
+ }
+}
 
 public class Bank {
-	int MINBAL=5000; //fields are static and final
-	static final int DAILY_LIMIT=25000;
-	public abstract void deposit(int amt) throws DepositLimitExceedsException;
-	void withdraw(int amt)throws InsufficientBalanceException; //public and abstract
+ // fields
+ int balance = 10000;
+ static final int MINBAL = 5000;
+ static final int DAILY_LIMIT = 25000;
 
+ // synchronized deposit method
+ public synchronized void deposit(int amount) throws DepositLimitExceedsException {
+     if (amount > DAILY_LIMIT) {
+         throw new DepositLimitExceedsException("Deposit limit exceeded! Maximum allowed: " + DAILY_LIMIT);
+     }
+
+     balance += amount;
+     System.out.println(Thread.currentThread().getName() + " deposited: " + amount);
+     System.out.println("Updated Balance: " + balance);
+ }
+
+ // synchronized withdraw method
+ public synchronized void withdraw(int amount) {
+     if (balance - amount < MINBAL) {
+         System.out.println(Thread.currentThread().getName() + " cannot withdraw " + amount + ". Minimum balance limit reached!");
+     } else {
+         balance -= amount;
+         System.out.println(Thread.currentThread().getName() + " withdrew: " + amount);
+         System.out.println("Updated Balance: " + balance);
+     }
+ }
 }
