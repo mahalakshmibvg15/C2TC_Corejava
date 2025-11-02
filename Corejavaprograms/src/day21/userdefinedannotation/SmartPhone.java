27383e630fee0357a195dd6c474a559652746bd9
mahal <mahal@kumaran1415>
2025-11-02 20:15:52 +0530

    commit

---
+++ Corejavaprograms/src/day21/userdefinedannotation/SmartPhone.java
@@ -0,0 +1,17 @@
+package day21.userdefinedannotation;
+
+public class SmartPhone {
+
+	import java.lang.annotation.Retention;
+	import java.lang.annotation.Target;
+	import java.lang.annotation.ElementType;
+	import java.lang.annotation.RetentionPolicy;
+
+	@Target(ElementType.TYPE)
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface SmartPhone {
+		String os() default "Android";
+
+		int version() default 1;	}
+
+}
---
+++ Corejavaprograms/src/day21/userdefinedannotation/SmartTV.java
@@ -0,0 +1,18 @@
+package day21.userdefinedannotation;
+
+public class SmartTV {
+
+	import java.lang.annotation.Retention;
+	import java.lang.annotation.Target;
+	import java.lang.annotation.ElementType;
+	import java.lang.annotation.RetentionPolicy;
+
+	@Target(ElementType.TYPE)
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface smartTv {
+		String os();
+
+		int width();
+
+		int height();	}
+
